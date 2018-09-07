d0b23c872494c8f4c8b9efcfc8dca0a886c798eb
davibrilhante <davisbrilhante@gmail>
2018-09-06 21:29:36 -0300

    atualizacao com parametros de entrada

--- shenzen-erbs.py
+++ shenzen-erbs.py
@@ -107,12 +107,27 @@ def hourAssign3(data):
         hours[int(hour[0])]+=1
     return hours
 
+def compHour(x):
+    if x.count(sys.argv[2])<>0:
+        return x 
+
 if __name__ == "__main__":
     sc = SparkContext()
     ### SAO 38938 CELULAS REGISTRADAS EM SHENZHEN!
-    ###out = open("shenzen-erbs.csv","w")
+    #out = open("shenzen-erbs.csv","w")
     
+    img = plt.imread("map-shenzhen.png")
+    fig, ax = plt.subplots()
+    ax.imshow(img, extent=[0,671,0,361])
+
+    zero_point = [22.428111, 113.724586]
+    endx = [22.423773, 114.650691]
+    xaxis = calcDist(zero_point, endx)
+    endy = [22.871618, 113.715715]
+    yaxis = calcDist(zero_point, endy)
     counter = 0
+
+
     erbs = []
     for i in ["454.csv","455.csv","460.csv"]:
         f = open(i)
@@ -122,18 +137,68 @@ if __name__ == "__main__":
             p = Point(float(line[7]),float(line[6]))
             if shenzhen.contains(p):
                 erbs.append(line)
-                ###out.write(j)
+                #out.write(j)
                 counter += 1
+                if sys.argv[4]=='0':
+                    r = calcDist(zero_point, [float(line[1][0]), float(line[1][1])])
+                    alpha = calcAngle(zero_point,  [float(line[1][0]), float(line[1][1])])
+                    xcoord = r*cos(alpha)*671/xaxis
+                    ycoord = r*sin(alpha)*371/yaxis
+                    plt.plot(ycoord, xcoord, marker='o', markersize=float(line[1][2])/200.0, color='red', alpha=0.5)
     print(counter)
     
     rddErbs = sc.parallelize(erbs)
     #print(rddErbs.count())
 
-    coordErbs = rddErbs.map(lambda x: (x[4],float(x[7]),float(x[6]),float(x[8])))
+    coordErbs = rddErbs.map(lambda x: (x[4],float(x[7]),float(x[6]),float(x[8]),x[0]))
     #print(coordErbs.take(10))
 
-    rddPhone = sc.textFile("PhoneData.csv")
-    rddPhoneStr = rddPhone.map(lambda x: x.strip('\n').split(",")).map(lambda x: (x[0],x[1],float(x[3]),float(x[2])))
+    key1 = sys.argv[2]
+
+    if sys.argv[1] == 'phone': 
+        cor = 'purple'
+        filename = "PhoneData.csv"
+        rddPhone = sc.textFile(filename)
+        rddPhoneStr = rddPhone.map(lambda x: x.strip('\n').split(","))\
+                    .filter(lambda x: x[1].split(":")[0] == key1)\
+                    .map(lambda x: (x[0],x[1],float(x[3]),float(x[2])))
+
+    elif sys.argv[1] == 'taxi': 
+        cor = 'red'
+        filename = "TaxiData.csv"
+        rddPhone = sc.textFile(filename)
+        rddPhoneStr = rddPhone.map(lambda x: x.strip('\n').split(","))\
+                    .filter(lambda x: x[1].split(":")[0] == key1)\
+                    .map(lambda x: (x[0],x[1],float(x[3]),float(x[2])))
+
+    elif sys.argv[1] == 'bus':
+        cor = 'blue'
+        filename = "BusData.csv"
+        rddPhone = sc.textFile(filename)
+        rddPhoneStr = rddPhone.map(lambda x: x.strip('\n').split(","))\
+                    .filter(lambda x: x[1].split(":")[0] == key1)\
+                    .map(lambda x: (x[0],x[1],float(x[4]),float(x[3])))
+
+    elif sys.argv[1] == 'truck': 
+        cor = 'green'
+        filename = "TruckData.csv"
+        rddPhone = sc.textFile(filename)
+        rddPhoneStr = rddPhone.map(lambda x: x.strip('\n').split(","))\
+                    .filter(lambda x: x[1].split(":")[0].split()[1] == key1)\
+                    .map(lambda x: (x[0],x[1].split()[1],float(x[3]),float(x[2])))
+    
+    print("\n\nCOUNT RDDPHONESTR ",rddPhoneStr.count())
+
+    #key2 = sys.argv[3]
+    '''if sys.argv[1]=='n':
+        rddPhone = sc.textFile("PhoneData.csv")
+        rddPhoneStr = rddPhone.map(lambda x: x.strip('\n').split(",")).map(lambda x: (x[0],x[1],float(x[3]),float(x[2])))
+    elif sys.argv[1]=='y':
+    rddPhone = sc.textFile("OLD-PhoneData.csv")
+    rddPhoneStr = rddPhone.map(lambda x: x.strip('\n').split(","))\
+                    .filter(lambda x: x[1].split(":")[0] == key1)\
+                    .map(lambda x: (x[0],x[1],float(x[3]),float(x[2])))
+    print("COUNT RDDPHONESTR ",rddPhoneStr.count())'''
 
     ### THIS RDD RETURNS THE THREE MOST HIGHEST BASE STATIONS TO AN USAR IN AN GIVEN SAMPLE
     ### SOMETHING LIKE: 
@@ -145,20 +210,25 @@ if __name__ == "__main__":
                     .reduceByKey(lambda x,y: sorted(x+y, key=lambda z: z[1])[:3])
                     #.reduceByKey(lambda x,y: min(x,y,key=lambda z: z[0][1]))
     #print(rddPhoneBase.take(10))
-
+    
+    ### THIS RDD CONTAINS A KEY VALUE PAIR WHERE THE KEY IS THE CELL ID AND THE VALUE IS  
+    ### A TUPLE WITH 24 ENTRIES WHICH ONE REPRESENTS THE HOURS OF THE DAY AND THE NUMBER
+    ### OF USERS CONNECTED AT THAT MOMENT
     rddBaseAccess = rddPhoneBase.flatMapValues(lambda x: x)\
                     .map(lambda x: (x[1][0],(x[0][0],x[0][1],x[1][1])))\
                     .reduceByKey(lambda x,y: x+y)\
                     .map(lambda x: (x[0],hourAssign(x[1])))
                     #.flatMapValues(hourAssign).
-
     #print(rddBaseAccess.take(10))
+
+    ### THIS RDD CONTAINS A KEY VALUE PAIR WHERE THE KEY IS THE CELL ID AND THE VALUE IS  
+    ### A TUPLE WITH 24 ENTRIES WHICH ONE REPRESENTS THE HOURS OF THE DAY AND EACH ONE
+    ### CONTAINS ANOTHER TUPLE WITH THE ID OF USERS CONNECTED AT THAT MOMENT
     rddBaseAccess2 = rddPhoneBase.flatMapValues(lambda x: x)\
                     .map(lambda x: (x[1][0],(x[0][0],x[0][1],x[1][1])))\
                     .reduceByKey(lambda x,y: x+y)\
                     .map(lambda x: (x[0],hourAssign2(x[1])))
                     #.flatMapValues(hourAssign).
-
     #print(rddBaseAccess.take(10))
 
     
@@ -172,7 +242,7 @@ if __name__ == "__main__":
                  .map(lambda x: (x[0],hourAssign3(x[1])))
     #print(rddHandover.take(10))
 
-    
+    '''    
     img = plt.imread("map-shenzhen.png")
     fig, ax = plt.subplots()
     ax.imshow(img, extent=[0,671,0,361])
@@ -182,10 +252,14 @@ if __name__ == "__main__":
     xaxis = calcDist(zero_point, endx)
     endy = [22.871618, 113.715715]
     yaxis = calcDist(zero_point, endy)
-
-    rddcoord = coordErbs.map(lambda x: (x[0],(x[1],x[2],x[3])))
+    #'''
+    rddcoord = coordErbs.map(lambda x: (x[0],(x[1],x[2],x[3],x[4])))
     
-    if sys.argv[1]=='1':
+    if sys.argv[4]=='1':
+        '''
+        PLOTA O ALCANCE DA ERB
+        '''
+        chave = 'cell-range'
         final=rddHandover.collect()
         for i in final:
             coord = rddcoord.lookup(i[0])[0]
@@ -194,9 +268,16 @@ if __name__ == "__main__":
             alpha = calcAngle(zero_point, coord[:2])
             xcoord = r*cos(alpha)*671/xaxis
             ycoord = r*sin(alpha)*371/yaxis
-            plt.plot(ycoord, xcoord, marker='o', markersize=coord[2]/200.0, color='red', alpha=0.5) #, markeredgecolor='none')
-
-    elif sys.argv[1]=='2':
+            if sys.argv[3]=='0': 
+                plt.plot(ycoord, xcoord, marker='o', markersize=coord[2]/200.0, color='dimgray', alpha=0.5)
+            elif sys.arv[3]==coord[3]: 
+                plt.plot(ycoord, xcoord, marker='o', markersize=coord[2]/200.0, color='dimgray', alpha=0.5)
+                
+    elif sys.argv[4]=='2':
+        '''
+        PLOTA O NUMERO DE USUARIOS CONECTADOS AQUELA ERB NAQUELA HORA
+        ''' 
+        chave = 'number-users'
         final = rddBaseAccess.collect()
         for i in final:
             print(i)
@@ -206,9 +287,18 @@ if __name__ == "__main__":
             alpha = calcAngle(zero_point, coord[:2])
             xcoord = r*cos(alpha)*671/xaxis
             ycoord = r*sin(alpha)*371/yaxis
-            plt.plot(ycoord, xcoord, marker='o', markersize=sum(i[1])/10.0 + 2, color='red', alpha=0.5) #, markeredgecolor='none')
+            r = sum(i[1])*10/sum(max(final, key=lambda x: sum(x[1]))[1])
+            #plt.plot(ycoord, xcoord, marker='o', markersize=r, color=cor, alpha=0.5) #, markeredgecolor='none')
+            if sys.argv[3]=='0': 
+                plt.plot(ycoord, xcoord, marker='o', markersize=r, color=cor, alpha=0.5) #, markeredgecolor='none')
+            elif sys.argv[3]==coord[3]: 
+                plt.plot(ycoord, xcoord, marker='o', markersize=r, color=cor, alpha=0.5) #, markeredgecolor='none')
 
-    elif sys.argv[1]=='3':
+    elif sys.argv[4]=='3':
+        '''
+        PLOTA O NUMERO DE HANDOVERS OCORRENDO NAQUELA ERB
+        '''
+        chave = 'number-handover'
         final=rddHandover.collect()
         for i in final:
             coord = rddcoord.lookup(i[0])[0]
@@ -217,20 +307,19 @@ if __name__ == "__main__":
             alpha = calcAngle(zero_point, coord[:2])
             xcoord = r*cos(alpha)*671/xaxis
             ycoord = r*sin(alpha)*371/yaxis
-            plt.plot(ycoord, xcoord, marker='o', markersize=sum(i[1])/2.0 + 2, color='red', alpha=0.5) #, markeredgecolor='none')
+            r = sum(i[1])*10/sum(max(final, key=lambda x: sum(x[1]))[1])
+            if sys.argv[3]=='0': 
+                plt.plot(ycoord, xcoord, marker='o', markersize=r, color=cor, alpha=0.5) #, markeredgecolor='none')
+            elif sys.argv[3]==coord[3]: 
+                plt.plot(ycoord, xcoord, marker='o', markersize=r, color=cor, alpha=0.5) #, markeredgecolor='none')
 
         
 
-    elif sys.argv[1]=='0':
-        #final=rddcoord.collect()
-        for i in erbs:
-            r = calcDist(zero_point, [float(i[7]), float(i[6])])
-            alpha = calcAngle(zero_point,  [float(i[7]), float(i[6])])
-            xcoord = r*cos(alpha)*671/xaxis
-            ycoord = r*sin(alpha)*371/yaxis
-            plt.plot(ycoord, xcoord, marker='o', markersize=float(i[8])/200.0, color='red', alpha=0.5) #, markeredgecolor='none')
-
-    elif sys.argv[1]=='4':
+    elif sys.argv[4]=='4':
+        '''
+        PLOTA OS USUARIOS NAQUELE HORARIO NO MAPA
+        '''
+        chave = 'user-positioning'
         erbs = []
         final = rddPhoneStr.collect() 
         for i in final:
@@ -238,18 +327,8 @@ if __name__ == "__main__":
             alpha = calcAngle(zero_point,  [float(i[2]), float(i[3])])
             xcoord = r*cos(alpha)*671/xaxis
             ycoord = r*sin(alpha)*371/yaxis
-            plt.plot(ycoord, xcoord, marker='o', markersize=2.0, color='black', alpha=0.5) #, markeredgecolor='none')
+            plt.plot(ycoord, xcoord, marker='o', markersize=2.0, color=cor, alpha=0.5) #, markeredgecolor='none')
             
 
     #print(xcoord,ycoord)
-    plt.savefig(sys.argv[1]+"-teste.png")  
-    '''
-    rddTaxi = sc.textFile("PhoneData.csv")
-    rddTaxiStr = rddTaxi.map(lambda x: x.strip('\n').split(",")).map(lambda x: (x[0],x[1],float(x[3]),float(x[2])))\
-                    .cartesian(coordErbs)\
-                    .filter(lambda x: calcDist((x[0][2],x[0][3]),(x[1][1],x[1][2])) < (x[1][3]))\
-                    .map(lambda x: ((x[0][0],x[0][1]),[(x[1][0],calcDist((x[0][2],x[0][3]),(x[1][1],x[1][2])))]))\
-                    .reduceByKey(lambda x,y: sorted(x+y, key=lambda z: z[1])[:3])\
-                    .map(lambda x: (x[0][0],[(x[0][1],x[1])]))\
-                    .reduceByKey(lambda x,y: sorted(x+y, key=lambda z: z[0].strip(":")))
-    '''
+    plt.savefig(sys.argv[1]+"-"+sys.argv[2]+"h-"+sys.argv[3]+"-"+sys.argv[4]+"-"+chave+".png")  
